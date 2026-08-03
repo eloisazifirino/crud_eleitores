7d7e19a7bec4b10c575c3ecfaaeacdcea7f5ec04
Eloisa Zifirino <eloisa.zeferino@aluno.senai.br>
2026-08-03 14:25:16 -0300

    feat: Adicionar eleitores e limpar formulário

--- src/app/components/cadastro-eleitores/cadastro-eleitores.ts
+++ src/app/components/cadastro-eleitores/cadastro-eleitores.ts
@@ -1,9 +1,39 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Eleitor } from './eleitor';
 
 @Component({
   selector: 'app-cadastro-eleitores',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './cadastro-eleitores.html',
   styleUrl: './cadastro-eleitores.css',
 })
-export class CadastroEleitores {}
+export class CadastroEleitores {
+  idEleitor : number = 0
+  numEleitor : number = 0
+  cidade : string = ''
+  listaEleitores : Eleitor[] = []
+
+addEleitor(){
+    console.log(this.numEleitor, '<-> ', 
+    this.cidade)
+    //INSTACIANDO A CLASSE ELEITOR    
+    let item = new Eleitor ()
+    item.idEleitor = this.listaEleitores.length + 1
+    item.numEleitor = this.numEleitor
+    item.cidade = this.cidade
+
+    this.listaEleitores.push(item)
+
+    this.numEleitor = 0
+    this.cidade = ''
+  }
+  
+    limparTudo(){
+      this.listaEleitores = []
+      this.numEleitor = 0
+      this.cidade = ''
+    }
+
+}
